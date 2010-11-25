402c49a3b8216da13196a1cc69466c188c0e5f15
Peter Bailey <peter.bailey@jwt.com>
2010-11-25 12:00:48 -0600

    Updated bootstrap for new filename

--- sdk/bootstrap.php
+++ sdk/bootstrap.php
@@ -3,7 +3,7 @@
 $here = dirname( __FILE__ ) . DIRECTORY_SEPARATOR;
 
 require( $here . 'GoogleGeocodeServiceV3.php' );
-require( $here . 'GoogleGeocodeResultV3.php' );
+require( $here . 'GoogleGeocodeResponseV3.php' );
 require( $here . 'exception' . DIRECTORY_SEPARATOR . 'GoogleGeocodeException.php' );
 require( $here . 'exception' . DIRECTORY_SEPARATOR . 'GoogleGeocodeCommunicatorException.php' );
 require( $here . 'communicator' . DIRECTORY_SEPARATOR . 'GoogleServiceCommunicator.php' );
